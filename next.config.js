88aae50b4f961079757396ffd47422ce228f4856
TrapGodBjornn <bshurd42@gmail.com>
2026-02-04 01:25:29 -0500

    Vercel images be fixed

--- next.config.js
+++ next.config.js
@@ -3,15 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
 
   images: {
-    unoptimized: false,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    unoptimized: true,
   },
 
   experimental: {
